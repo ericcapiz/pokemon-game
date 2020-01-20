01a54dd7adb13370c7fd66817e265b6bfe9c5b48
ericcapiz <53915408+ericcapiz@users.noreply.github.com>
2020-01-20 15:13:08 -0600

    attack moves and health calculated

--- public/main.js
+++ public/main.js
@@ -1,39 +1,123 @@
+var pokemonDB = [{
+    name: 'charmander',
+    type: 'fire',
+    hp: 39,
+    attack: 52,
+    defense: 43,
+    level: 1,
+    img: 'http://www.smogon.com/dex/media/sprites/xy/charmander.gif'
+  },
+  {
+    name: 'bulbasaur',
+    type: 'grass',
+    hp: 45,
+    attack: 49,
+    defense: 49,
+    level: 1,
+    img: 'http://www.smogon.com/dex/media/sprites/xy/bulbasaur.gif'
+  },
+  {
+    name: 'squirtle',
+    type: 'water',
+    hp: 44,
+    attack: 48,
+    defense: 65,
+    level: 1,
+    img: 'http://www.smogon.com/dex/media/sprites/xy/squirtle.gif'
+  }
+];
+
 var gameState = {
-	userPokemon: '',
-	rivalPokemon:''
+  userPokemon: '',
+  rivalPokemon: ''
 }
 //gameState to keep track of what pokemon user/cpu selected
 console.log(gameState)
-//##while loop for user to select a pokemon character###\\
+//creating elements
 var pokemonsEl = document
-	.querySelector('.select-screen')
-	.querySelectorAll('.character');
+  .querySelector('.select-screen')
+  .querySelectorAll('.character');
 console.log(pokemonsEl);
 var battleScreenEl = document.getElementById('battle-screen')
-// ###create variable to use the battle-screen id##
+var attackBtnsEl = document.getElementById('battle-screen').querySelectorAll('.attack')
 
-i=0;
-while(i < pokemonsEl.length){
-	pokemonsEl[i].onclick = function(){
-		var pokemonName = this.dataset.pokemon
-		gameState.userPokemon = pokemonName
-
-		cpuPick()
-		battleScreenEl.classList.toggle('active')
-		//##once cpu picks, screen switches to battle screen
-		console.log(gameState)
-	}
-	i++
-}
 
 //##while loop for user to select a pokemon character###\\
-// ###functions to have cpu randomly select a pokemon###
-function randomNumber(min, max){
-	return Math.floor(Math.random() * (max - min)) + min;
+i = 0;
+while (i < pokemonsEl.length) {
+  pokemonsEl[i].onclick = function() {
+    var pokemonName = this.dataset.pokemon
+    var player1Img = document.querySelector('.player1').getElementsByTagName('img')
+		var player2Img = document.querySelector('.player2').getElementsByTagName('img')
+
+    gameState.userPokemon = pokemonName
+
+    cpuPick()
+    battleScreenEl.classList.toggle('active')
+    //##once cpu picks, screen switches to battle screen
+//## created variables to pull imgs from pokemonDB
+    gameState.currentPokemon = pokemonDB.filter(function(pokemon) {
+      return pokemon.name == gameState.userPokemon
+    })
+
+    gameState.currentRivalPokemon = pokemonDB.filter(function(pokemon) {
+      return pokemon.name == gameState.rivalPokemon
+    })
+//## created variables to pull imgs from pokemonDB
+//##once user/cpu selects a pokemon, imgs will show on battle screen
+    player1Img[0].src = gameState.currentPokemon[0].img
+		player2Img[0].src = gameState.currentRivalPokemon[0].img
+//##once user/cpu selects a pokemon, imgs will show on battle screen
+	gameState.currentPokemon[0].health = calculateHealth(gameState.currentPokemon)
+
+  }
+  i++
+}
+var a = 0;
+while (a < attackBtnsEl.length){
+	attackBtnsEl[a].onclick = function(){
+		var attackName = this.dataset.attack
+		gameState.currentUserAttack = attackName
+		play(attackName, cpuAttack())
+	}
+	a++
+}
+var cpuAttack = function(){
+	var attacks = ['rock', 'paper', 'scissors']
+
+	return attacks[randomNumber(0, 3)]
 }
 
-function cpuPick(){
-	gameState.rivalPokemon = pokemonsEl[randomNumber(0, 3)].dataset.pokemon
+var calculateHealth = function(user){
+	return ((0.2 * Math.sqrt(user[0].level)) * user[0].defense) * user[0].hp
+}
+
+var play = function(userAttack,cpuAttack){
+	switch(userAttack){
+		case'rock':
+			if(cpuAttack == 'paper'){
+				console.log('paper wins')
+			}
+			if(cpuAttack == 'scissors'){
+				console.log('rock wins')
+			}
+			if(cpuAttack == 'rock'){
+				console.log('draw')
+			}
+		break;
+		case 'paper':
+		break;
+		case 'scissors':
+		break;
+	}
+}
+// ###functions to have cpu randomly select a pokemon###
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function cpuPick() {
+  gameState.rivalPokemon = pokemonsEl[randomNumber(0, 3)].dataset.pokemon
 }
 
 // ###functions to have cpu randomly select a pokemon###
@@ -41,22 +125,7 @@ function cpuPick(){
 
 // pokemon
 // create data for 3 different pokemons, with their names, type, weaknesses, health, and attack moves(name, attack stat, maximum)
-// var pokemons = [
-// 	{
-// 		name: 'charmander',
-// 		type: 'fire',
-// 		attack: 52,
-// 		stamina: 39,
-// 		level: 1
-// 	},
-// 	{
-// 		name: 'charmander',
-// 		type: 'fire',
-// 		attack: 52,
-// 		stamina: 39,
-// 		level: 1
-// 	}
-// ];
+
 //
 // var attack = 20;
 // var level = 10;
